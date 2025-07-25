6128822b8354a8f9c9e612959d26ca612c6cd466
ab-sc4t <ab.ayush2612@gmail.com>
2025-07-25 19:33:27 +0530

    deploying

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/leetcode-patterns/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
